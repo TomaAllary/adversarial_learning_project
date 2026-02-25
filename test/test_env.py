e9b94d21ecc33e33edd7a2d7f9ce0db4a68193e4
allaryt <poki114poki114@outlook.com>
2026-02-25 16:57:49 -0500

    Repo creation - First draft

---
+++ test/test_env.py
@@ -0,0 +1,9 @@
+from pettingzoo_env import CustomEnvironment
+
+from pettingzoo.test import parallel_api_test, api_test
+
+if __name__ == "__main__":
+    env = CustomEnvironment()
+    #api_test(env, num_cycles=1_000_000, verbose_progress=True)
+    parallel_api_test(env, num_cycles=1_000_000)
+    print("hello")
