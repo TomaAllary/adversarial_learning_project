95722741c9dd6d117afb05294841de0cdb351843
lofeodo <daniel.lofeodo@gmail.com>
2026-04-23 11:44:33 -0400

    fix import bug

--- new_rnad.py
+++ new_rnad.py
@@ -24,7 +24,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import gym
+import gymnasium as gym
 
 
 # ---------------------------------------------------------------------------
